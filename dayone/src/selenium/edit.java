e28dbfb6b80c4f67d7df997745de813f03774c65
PrasanthRA <PrasanthRA@DESKTOP-NJQ6C4S>
2021-04-03 15:59:19 +0530

    first commit

---
+++ dayone/src/selenium/edit.java
@@ -0,0 +1,43 @@
+package selenium;
+
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class edit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver", "F:\\Friends Files\\suresh\\software\\chromedriver1\\chromedriver_win32\\chromedriver.exe");
+		ChromeOptions chromeoption = new ChromeOptions();
+		chromeoption.addArguments("--start-maximized");
+		WebDriver driver = new ChromeDriver(chromeoption);
+		driver.get("http://www.leafground.com/");
+		
+		driver.findElement(By.xpath("//*[@id=\"post-153\"]/div[2]/div/ul/li[1]/a/img")).click();
+		
+		WebElement emailTxtBox = driver.findElement(By.id("email"));
+		emailTxtBox.sendKeys("sureshsure33@gmail.com");
+		
+		WebElement appandTxtBox = driver.findElement(By.xpath("//*[@id=\"contentblock\"]/section/div[2]/div/div/input"));
+		appandTxtBox.sendKeys("update");
+		
+		WebElement getvalueTxtBox = driver.findElement(By.xpath("//*[@id=\"contentblock\"]/section/div[3]/div/div/input"));
+		String getvalue = getvalueTxtBox.getAttribute("value");
+		System.out.println("the value of text "+getvalue);
+		
+		WebElement clearTxtBox = driver.findElement(By.xpath("//*[@id=\"contentblock\"]/section/div[4]/div/div/input"));
+		clearTxtBox.clear();
+		
+		WebElement disableTxtBox = driver.findElement(By.xpath("//*[@id=\"contentblock\"]/section/div[5]/div/div/input"));
+		System.out.println(disableTxtBox.isEnabled());
+	}
+
+
+}
---
+++ dayone/src/selenium/sample.java
@@ -0,0 +1,11 @@
+package selenium;
+
+public class sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("sureshriya");
+		System.err.println("invalid message");
+	}
+
+}
